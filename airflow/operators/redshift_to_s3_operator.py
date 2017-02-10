474dbba68f85401c0e7301f8825b135e043fb2c5
Ubuntu <ubuntu@ip-10-0-2-212.us-west-2.compute.internal>
2017-02-10 21:57:32 +0000

    redshift operator update

--- airflow/operators/redshift_to_s3_operator.py
+++ airflow/operators/redshift_to_s3_operator.py
@@ -67,6 +67,15 @@ class RedshiftToS3Transfer(BaseOperator):
         self.autocommit = autocommit
         self.parameters = parameters
 
+    def column_mapping(self, columns):
+        ret_val = []
+        for a in columns:
+            if a[1] == "boolean":
+                ret_val.append("CAST((CASE when {0} then \\'1\\' else \\'0\\' end) AS TEXT) AS {0}".format(a[0], a[1]))
+            else:
+                ret_val.append("CAST({0} AS TEXT) AS {0}".format(a[0]))
+        return ', '.join(ret_val)
+                
     def execute(self, context):
         self.hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.s3 = S3Hook(s3_conn_id=self.s3_conn_id)
@@ -75,7 +84,7 @@ class RedshiftToS3Transfer(BaseOperator):
 
         logging.info("Retrieving headers from %s.%s..." % (self.schema, self.table))
 
-        columns_query = """SELECT column_name
+        columns_query = """SELECT column_name, data_type
                             FROM information_schema.columns
                             WHERE table_schema = '{0}'
                             AND   table_name = '{1}'
@@ -85,25 +94,41 @@ class RedshiftToS3Transfer(BaseOperator):
         cursor = self.hook.get_conn().cursor()
         cursor.execute(columns_query)
         rows = cursor.fetchall()
-        columns = map(lambda row: row[0], rows)
-        column_names = (', ').join(map(lambda c: "\\'{0}\\'".format(c), columns))
-        # column_castings = (', ').join(map(lambda c: "CAST({0} AS text) AS {0}".format(c),
-        #                                   columns))
+        print rows
+        column = ', '.join(map(lambda row: row[0], rows))
+        columns = map(lambda row: [row[0], row[1]], rows)
+        column_names = (', ').join(map(lambda c: "\\'{0}\\'".format(c[0]), columns))
+        column_castings = self.column_mapping(columns) # (', ').join(map(lambda c: "CAST({0} AS {1}) AS {0}".format(c[0], c[1]),
+        #                 columns))
 
         date_dir = datetime.today().strftime("%Y%m%d")
+        
         unload_query = """
                         UNLOAD ('SELECT {0}
-                        UNION ALL
-                        SELECT *
+                        UNION 
+                        SELECT {1}
                         FROM {2}.{3}')
                         TO 's3://{4}/{5}/{9}/{3}/{3}_'
                         with
                         credentials 'aws_access_key_id={6};aws_secret_access_key={7}'
                         {8}
                         delimiter '|' addquotes escape allowoverwrite;
-                        """.format(column_names, columns, self.schema, self.table,
+                        """.format(column_names, column_castings, self.schema, self.table,
                                    self.s3_bucket, self.s3_key, a_key, s_key, unload_options, date_dir)
-
+        print unload_query
         logging.info('Executing UNLOAD command...')
         self.hook.run(unload_query, self.autocommit)
         logging.info("UNLOAD command complete...")
+
+
+
+
+# UNLOAD ('SELECT \'post_id\', \'channel_id\', \'post_title\', \'message_text\', \'d_date\', \'post_type\', \'updated_date\', \'embedded_media_url\', \'post_image_url\', \'facebook_object_id\', \'timeline_visibility\', \'is_published\', \'like_count\', \'comment_count\', \'share_count\', \'scraped_date\', \'uploaded_date\', \'row_num\'
+#                         UNION
+#                         SELECT CAST(post_id AS character varying) AS post_id, CAST(channel_id AS character varying) AS channel_id, CAST(post_title AS character varying) AS post_title, CAST(message_text AS character varying) AS message_text, CAST(created_date AS text) AS created_date, CAST(post_type AS character varying) AS post_type, CAST(updated_date AS text) AS updated_date, CAST(embedded_media_url AS character varying) AS embedded_media_url, CAST(post_image_url AS character varying) AS post_image_url, CAST(facebook_object_id AS text) AS facebook_object_id, CAST(timeline_visibility AS character varying) AS timeline_visibility, CAST((CASE when is_published then \'1\' else \'0\' end) as text) AS is_published, CAST(like_count AS text) AS like_count, CAST(comment_count AS text) AS comment_count, CAST(share_count AS text) AS share_count, CAST(scraped_date AS text) AS scraped_date, CAST(uploaded_date AS text) AS uploaded_date, CAST(row_num AS text) AS row_num
+#                         FROM brandsoulmates.view_facebook_post_latest')
+# TO 's3://ayz-users/brandsoulmates/20170210/view_facebook_post_latest/view_facebook_post_latest_'
+# with
+# credentials 'aws_access_key_id=AKIAI6637BNKS4AMVIHA;aws_secret_access_key=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'
+
+# delimiter '|' addquotes escape allowoverwrite;
